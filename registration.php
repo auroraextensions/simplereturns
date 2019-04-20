e8b1fcc4e2d5dff2c9c89bbd545569af2e94c275
Nickolas Burr <nickolasburr@gmail.com>
2019-04-20 15:15:50 -0400

    Add composer.json, registration.php

---
+++ registration.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * registration.php
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Aurora Extensions EULA,
+ * which is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/returnlabels/LICENSE.txt
+ *
+ * @package       AuroraExtensions_ReturnLabels
+ * @copyright     Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
+ * @license       Aurora Extensions EULA
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'AuroraExtensions_ReturnLabels',
+    __DIR__
+);
