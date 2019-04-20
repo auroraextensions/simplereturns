ac49497bdf5bd46adbc31ac93dd8641ad206078f
Nickolas Burr <nickolasburr@gmail.com>
2019-04-20 15:27:56 -0400

    Add Shared\DictionaryInterface

---
+++ Shared/DictionaryInterface.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * DictionaryInterface.php
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Aurora Extensions EULA,
+ * which is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/returnlabels/LICENSE.txt
+ *
+ * @package       AuroraExtensions_ReturnLabels
+ * @copyright     Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
+ * @license       Aurora Extensions EULA
+ */
+namespace AuroraExtensions\ReturnLabels\Shared;
+
+interface DictionaryInterface
+{
+    /** @constant string XML_LAYOUT_HANDLE_NOROUTE */
+    const XML_LAYOUT_HANDLE_NOROUTE = 'returns_noroute';
+}
