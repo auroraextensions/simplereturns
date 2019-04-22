c5e60a0c1aff30e6dcac3822bd35c5567ff02a50
Nickolas Burr <nickolasburr@gmail.com>
2019-04-22 16:52:06 -0400

    Add router, controllers

---
+++ Controller/Label/Index.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ * Index.php
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Aurora Extensions EULA,
+ * which is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/returns/LICENSE.txt
+ *
+ * @package       AuroraExtensions_Returns
+ * @copyright     Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
+ * @license       Aurora Extensions EULA
+ */
+namespace AuroraExtensions\Returns\Controller\Label;
+
+use AuroraExtensions\Returns\{
+    Helper\Action as ActionHelper,
+    Model\Label\Processor,
+    Model\Label as LabelModel,
+    Model\Orders as OrdersModel,
+    Shared\Action\Redirector,
+    Shared\DictionaryInterface,
+    ViewModel\Label as ViewModel
+};
+
+use Magento\Framework\{
+    App\Action\Action,
+    App\Action\Context,
+    App\Action\HttpGetActionInterface,
+    Controller\Result\Redirect as ResultRedirect,
+    Data\Form\FormKey\Validator as FormKeyValidator,
+    View\Result\PageFactory
+};
+
+class Index extends Action implements HttpGetActionInterface, DictionaryInterface
+{
+    /** @see AuroraExtensions\Returns\Shared\Action\Redirector */
+    use Redirector {
+        Redirector::__initialize as protected;
+    }
+
+    /** @property FormKeyValidator $formKeyValidator */
+    protected $formKeyValidator;
+
+    /** @property LabelModel $labelModel */
+    protected $labelModel;
+
+    /** @property OrdersModel $ordersModel */
+    protected $ordersModel;
+
+    /** @property Processor $processor */
+    protected $processor;
+
+    /** @property PageFactory $resultPageFactory */
+    protected $resultPageFactory;
+
+    /** @property ViewModel $viewModel */
+    protected $viewModel;
+
+    /**
+     * @param Context $context
+     * @param FormKeyValidator $formKeyValidator
+     * @param LabelModel $labelModel
+     * @param PageFactory $resultPageFactory
+     * @param OrdersModel $ordersModel
+     * @param Processor $processor
+     * @param ViewModel $viewModel
+     * @return void
+     */
+    public function __construct(
+        Context $context,
+        FormKeyValidator $formKeyValidator,
+        LabelModel $labelModel,
+        PageFactory $resultPageFactory,
+        OrdersModel $ordersModel,
+        Processor $processor,
+        ViewModel $viewModel
+    ) {
+        parent::__construct($context);
+        $this->__initialize();
+        $this->formKeyValidator = $formKeyValidator;
+        $this->labelModel = $labelModel;
+        $this->resultPageFactory = $resultPageFactory;
+        $this->ordersModel = $ordersModel;
+        $this->processor = $processor;
+        $this->viewModel = $viewModel;
+    }
+
+    /**
+     * Execute returns_label_index action.
+     *
+     * @return Redirect|Page
+     */
+    public function execute()
+    {
+        /** @var Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+
+        /** @var Magento\Framework\App\RequestInterface $request */
+        $request = $this->getRequest();
+
+        /** @var string|null $orderId */
+        $orderId = $request->getParam(self::PARAM_ORDER_ID, null);
+
+        /** @var string|null $protectCode */
+        $protectCode = $request->getParam(self::PARAM_PROTECT_CODE, null);
+
+        if ($orderId !== null && $protectCode !== null) {
+            /** @var OrderInterface[] $orders */
+            $orders = $this->ordersModel->getOrdersByIncrementIdAndProtectCode($orderId, $protectCode);
+
+            if (!empty($orders)) {
+                /** @var OrderInterface $order */
+                $order = $orders[0];
+
+                /* View model requires order for cache lookup. */
+                $this->viewModel->setOrder($order);
+
+                /** @var string $cacheKey */
+                $cacheKey = $this->labelModel->getCacheKey($order);
+
+                if (($this->labelModel->isCacheEnabled() && $this->labelModel->hasCachedImage($cacheKey))
+                    || $this->processor->requestReturnLabel($order)
+                ) {
+                    /** @var Magento\Framework\View\Element\AbstractBlock|bool $block */
+                    $block = $resultPage->getLayout()->getBlock(self::BLOCK_RETURNS_LABEL_INDEX);
+
+                    if ($block) {
+                        $block->setData('view_model', $this->viewModel);
+                    }
+
+                    return $resultPage;
+                }
+            }
+        }
+
+        return $this->getRedirect(self::ROUTE_RETURNS_LABEL_ORDERS);
+    }
+}
---
+++ Controller/Label/Orders.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * Orders.php
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Aurora Extensions EULA,
+ * which is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/returns/LICENSE.txt
+ *
+ * @package       AuroraExtensions_Returns
+ * @copyright     Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
+ * @license       Aurora Extensions EULA
+ */
+namespace AuroraExtensions\Returns\Controller\Label;
+
+use AuroraExtensions\Returns\{
+    Helper\Action as ActionHelper,
+    Model\Orders as OrdersModel,
+    Shared\DictionaryInterface,
+    ViewModel\Orders as ViewModel
+};
+
+use Magento\Framework\{
+    App\Action\Action,
+    App\Action\Context,
+    App\Action\HttpGetActionInterface,
+    App\Request\DataPersistorInterface,
+    View\Result\PageFactory
+};
+
+class Orders extends Action implements HttpGetActionInterface, DictionaryInterface
+{
+    /** @property DataPersistorInterface $dataPersistor */
+    protected $dataPersistor;
+
+    /** @property OrdersModel $ordersModel */
+    protected $ordersModel;
+
+    /** @property PageFactory $resultPageFactory */
+    protected $resultPageFactory;
+
+    /** @property ViewModel $viewModel */
+    protected $viewModel;
+
+    /**
+     * @param Context $context
+     * @param DataPersistorInterface $dataPersistor
+     * @param OrdersModel $ordersModel
+     * @param PageFactory $resultPageFactory
+     * @param ViewModel $viewModel
+     * @return void
+     */
+    public function __construct(
+        Context $context,
+        DataPersistorInterface $dataPersistor,
+        OrdersModel $ordersModel,
+        PageFactory $resultPageFactory,
+        ViewModel $viewModel
+    ) {
+        parent::__construct($context);
+        $this->dataPersistor = $dataPersistor;
+        $this->ordersModel = $ordersModel;
+        $this->resultPageFactory = $resultPageFactory;
+        $this->viewModel = $viewModel;
+    }
+
+    /**
+     * Execute returns_label_orders action.
+     *
+     * @return Page
+     */
+    public function execute()
+    {
+        /** @var Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+
+        /** @var array $data */
+        $data = $this->dataPersistor->get(self::DATA_PERSISTOR_KEY);
+        $this->dataPersistor->clear(self::DATA_PERSISTOR_KEY);
+
+        if ($data) {
+            $this->viewModel->setData($data);
+
+            /** @var string|null $email */
+            $email = $data[self::PARAM_EMAIL] ?? null;
+
+            /** @var string|null $orderId */
+            $orderId = $data[self::PARAM_ORDER_ID] ?? null;
+
+            /** @var string|null $zipCode */
+            $zipCode = $data[self::PARAM_ZIP_CODE] ?? null;
+
+            if ($email !== null && $zipCode !== null) {
+                /** @var array $orders */
+                $orders = $this->ordersModel->getOrdersByCustomerEmailAndZipCode($email, $zipCode);
+
+                $this->viewModel->setData('orders', $orders);
+            } elseif ($orderId !== null && $zipCode !== null) {
+                /** @var array $orders */
+                $orders = $this->ordersModel->getOrdersByIncrementIdAndZipCode($orderId, $zipCode);
+
+                $this->viewModel->setData('orders', $orders);
+            }
+
+            /** @var Magento\Framework\View\Element\AbstractBlock|bool $block */
+            $block = $resultPage->getLayout()->getBlock(self::BLOCK_RETURNS_LABEL_ORDERS);
+
+            if ($block) {
+                $block->setData('view_model', $this->viewModel);
+            }
+        }
+
+        return $resultPage;
+    }
+}
