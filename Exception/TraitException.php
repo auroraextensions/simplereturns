ee6fbcb547ac464327afa1c2073bb1fb544ac3b0
Nickolas Burr <nickolasburr@gmail.com>
2019-04-22 18:06:23 -0400

    Add TraitException class

---
+++ Exception/TraitException.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * TraitException.php
+ *
+ * Exception class specific to invalid trait usage.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Aurora Extensions EULA,
+ * which is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/returns/LICENSE.txt
+ *
+ * @package       AuroraExtensions_Returns
+ * @copyright     Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
+ * @license       Aurora Extensions EULA
+ */
+namespace AuroraExtensions\Returns\Exception;
+
+class TraitException extends \Exception
+{
+}
