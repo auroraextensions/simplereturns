ba6365dab259200b26bde37e9063f482e25b8773
Nickolas Burr <nickolasburr@gmail.com>
2019-04-22 17:49:47 -0400

    Add NOL to Action.php

--- Helper/Action.php
+++ Helper/Action.php
@@ -3,6 +3,18 @@
  * Action.php
  *
  * Helper class for handling action-related parsing tasks.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Aurora Extensions EULA,
+ * which is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/returns/LICENSE.txt
+ *
+ * @package       AuroraExtensions_Returns
+ * @copyright     Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
+ * @license       Aurora Extensions EULA
  */
 namespace AuroraExtensions\Returns\Helper;
 
