1c1faac5e10f9229a8540b7743cc4a75dd4d663d
Nickolas Burr <nickolasburr@gmail.com>
2019-05-26 02:33:09 -0400

    Add SimpleReturnRepository resource model

---
+++ Model/ResourceModel/SimpleReturnRepository.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * SimpleReturnRepository.php
+ *
+ * RMA repository model.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Aurora Extensions EULA,
+ * which is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/simplereturns/LICENSE.txt
+ *
+ * @package       AuroraExtensions_SimpleReturns
+ * @copyright     Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
+ * @license       Aurora Extensions EULA
+ */
+declare(strict_types=1);
+
+namespace AuroraExtensions\SimpleReturns\Model\ResourceModel;
+
+use AuroraExtensions\SimpleReturns\{
+    Api\SimpleReturnRepositoryInterface,
+    Api\Data\SimpleReturnInterface,
+    Api\Data\SimpleReturnInterfaceFactory,
+    Api\Data\SimpleReturnSearchResultsInterfaceFactory,
+    Exception\ExceptionFactory,
+    Model\SimpleReturn as SimpleReturnAdapter,
+    Model\ResourceModel\SimpleReturn as SimpleReturnResource,
+    Shared\ModuleComponentInterface
+};
+
+use Magento\{
+    Framework\Api\SearchCriteriaInterface,
+    Framework\Api\SortOrder,
+    Framework\Exception\NoSuchEntityException,
+    Sales\Api\Data\OrderInterface
+};
+
+class SimpleReturnRepository implements SimpleReturnRepositoryInterface, ModuleComponentInterface
+{
+    /** @property SimpleReturn\CollectionFactory $collectionFactory */
+    protected $collectionFactory;
+
+    /** @property ExceptionFactory $exceptionFactory */
+    protected $exceptionFactory;
+
+    /** @property SimpleReturnSearchResultsInterfaceFactory $searchResultsFactory */
+    protected $searchResultsFactory;
+
+    /** @property SimpleReturnInterfaceFactory $simpleReturnFactory */
+    protected $simpleReturnFactory;
+
+    /** @property SimpleReturnResource $simpleReturnResource */
+    protected $simpleReturnResource;
+
+    /**
+     * @param SimpleReturnInterfaceFactory $simpleReturnFactory
+     * @param SimpleReturnResource $simpleReturnResource
+     * @param SimpleReturn\CollectionFactory $collectionFactory
+     * @param SimpleReturnSearchResultsInterfaceFactory $searchResultsFactory
+     * @param ExceptionFactory $exceptionFactory
+     * @return void
+     */
+    public function __construct(
+        SimpleReturnInterfaceFactory $simpleReturnFactory,
+        SimpleReturnResource $simpleReturnResource,
+        SimpleReturn\CollectionFactory $collectionFactory,
+        SimpleReturnSearchResultsInterfaceFactory $searchResultsFactory,
+        ExceptionFactory $exceptionFactory
+    ) {
+        $this->simpleReturnFactory = $simpleReturnFactory;
+        $this->simpleReturnResource = $simpleReturnResource;
+        $this->collectionFactory = $collectionFactory;
+        $this->searchResultsFactory = $searchResultsFactory;
+        $this->exceptionFactory = $exceptionFactory;
+    }
+
+    /**
+     * Get SimpleReturn RMA for order.
+     *
+     * @param OrderInterface $order
+     * @return SimpleReturnInterface
+     */
+    public function get(OrderInterface $order): SimpleReturnInterface
+    {
+        /** @var SimpleReturnAdapter $simpleReturn */
+        $simpleReturn = $this->simpleReturnFactory->create();
+        $this->simpleReturnResource->load(
+            $simpleReturn,
+            $order->getId(),
+            self::SQL_COLUMN_SIMPLERETURN_ORDER_ID_FIELD
+        );
+
+        if (!$simpleReturn->getId()) {
+            throw $this->exceptionFactory->create(
+                __('Unable to locate RMA information for the requested order.')
+            );
+        }
+
+        return $simpleReturn;
+    }
+}
