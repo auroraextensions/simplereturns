3db98b4fe38e25bc04189643893e77d45f16432f
Nickolas Burr <nickolasburr@gmail.com>
2019-05-27 20:51:47 -0400

    Set strict_types=1 in SimpleReturn resource model

--- Model/ResourceModel/SimpleReturn.php
+++ Model/ResourceModel/SimpleReturn.php
@@ -2,8 +2,20 @@
 /**
  * SimpleReturn.php
  *
- * SimpleReturn RMA resource model.
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Aurora Extensions EULA,
+ * which is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/simplereturns/LICENSE.txt
+ *
+ * @package       AuroraExtensions_SimpleReturns
+ * @copyright     Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
+ * @license       Aurora Extensions EULA
  */
+declare(strict_types=1);
+
 namespace AuroraExtensions\SimpleReturns\Model\ResourceModel;
 
 use AuroraExtensions\SimpleReturns\Shared\ModuleComponentInterface;
