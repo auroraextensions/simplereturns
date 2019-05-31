a0955a8c53de914d25c0c06ab06bfc8714002449
Nickolas Burr <nickolasburr@gmail.com>
2019-05-31 13:42:59 -0400

    Convert Model\Label to data model

--- Model/Label.php
+++ Model/Label.php
@@ -2,8 +2,6 @@
 /**
  * Label.php
  *
- * Return labels adapter model.
- *
  * NOTICE OF LICENSE
  *
  * This source file is subject to the Aurora Extensions EULA,
@@ -16,157 +14,63 @@
  * @copyright     Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
  * @license       Aurora Extensions EULA
  */
+declare(strict_types=1);
+
 namespace AuroraExtensions\SimpleReturns\Model;
 
-use AuroraExtensions\{
-    Cache\Model\Type as LabelCache,
-    Returns\Shared\ModuleComponentInterface
+use AuroraExtensions\SimpleReturns\{
+    Api\Data\LabelInterface,
+    Shared\ModuleComponentInterface
 };
+use Magento\Framework\Model\AbstractModel;
 
-use Magento\{
-    Framework\App\Cache\StateInterface,
-    Framework\Serialize\Serializer\Json,
-    Sales\Api\Data\OrderInterface
-};
-
-class Label implements ModuleComponentInterface
+class Label extends AbstractModel implements
+    LabelInterface,
+    ModuleComponentInterface
 {
-    /** @property LabelCache $cache */
-    protected $cache;
-
-    /** @property StateInterface $cacheState */
-    protected $cacheState;
-
-    /** @property string|null $image */
-    protected $image;
-
-    /** @property Json $serializer */
-    protected $serializer;
-
     /**
-     * @param LabelCache $cache
-     * @param StateInterface $cacheState
-     * @param Json $serializer
      * @return void
      */
-    public function __construct(
-        LabelCache $cache,
-        StateInterface $cacheState,
-        Json $serializer
-    ) {
-        $this->cache = $cache;
-        $this->cacheState = $cacheState;
-        $this->serializer = $serializer;
-    }
-
-    /**
-     * Check if cache is enabled.
-     *
-     * @return bool
-     */
-    public function isCacheEnabled(): bool
+    public function _construct()
     {
-        return $this->cacheState->isEnabled(LabelCache::TYPE_IDENTIFIER);
+        $this->_init(ResourceModel\Label::class);
     }
 
     /**
-     * Get cache instance.
-     *
-     * @return LabelCache
-     */
-    protected function getCache()
-    {
-        return $this->cache;
-    }
-
-    /**
-     * Get label image.
-     *
      * @return string|null
      */
-    public function getImage()
+    public function getImage(): ?string
     {
-        return $this->image;
+        return $this->getData('image');
     }
 
     /**
-     * Get cache key from order data.
-     *
-     * @param OrderInterface $order
-     * @return string
-     */
-    public function getCacheKey(OrderInterface $order): string
-    {
-        /** @var array $parts */
-        $parts = [
-            self::LABEL_CACHE_ID,
-            $order->getStoreId(),
-            $order->getIncrementId(),
-            $order->getProtectCode(),
-        ];
-
-        return implode('_', $parts);
-    }
-
-    /**
-     * Get label image from cache storage.
-     *
-     * @return mixed
-     */
-    public function getCachedImage(string $cacheKey)
-    {
-        if ($cache = $this->getCache()->load($cacheKey)) {
-            return $this->serializer->unserialize($cache);
-        }
-
-        return null;
-    }
-
-    /**
-     * Check if there is cached return label for order.
-     *
-     * @param string $cacheKey
-     * @return bool
-     */
-    public function hasCachedImage(string $cacheKey): bool
-    {
-        if ($this->getCachedImage($cacheKey) !== null) {
-            return true;
-        }
-
-        return false;
-    }
-
-    /**
-     * Set label image in cache.
-     *
-     * @param string $cacheKey
-     * @param mixed $image
+     * @param string|null $image
      * @return $this
      */
-    public function setCachedImage(string $cacheKey, $image)
+    public function setImage(?string $image): LabelInterface
     {
-        /* Serialize data and save to cache. */
-        $this->getCache()->save(
-            $this->serializer->serialize($image),
-            $cacheKey,
-            [
-                LabelCache::CACHE_TAG,
-            ]
-        );
+        $this->setData('image', $image);
 
         return $this;
     }
 
     /**
-     * Set label image.
-     *
-     * @param string $image
+     * @return string
+     */
+    public function getTrackingNumber(): string
+    {
+        return $this->getData('tracking_number');
+    }
+
+    /**
+     * @param string $trackingNumber
      * @return $this
      */
-    public function setImage($image)
+    public function setTrackingNumber(string $trackingNumber): LabelInterface
     {
-        $this->image = $image;
+        $this->setData('tracking_number', $trackingNumber);
+
         return $this;
     }
 }
