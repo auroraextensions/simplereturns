b80d91c78097a578c6299c91f926af812d5458e6
Nickolas Burr <nickolasburr@gmail.com>
2019-05-31 12:59:04 -0400

    Add SimpleReturn data model

---
+++ Model/SimpleReturn.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * SimpleReturn.php
+ *
+ * Return labels adapter model.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Aurora Extensions EULA,
+ * which is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/simplereturns/LICENSE.txt
+ *
+ * @package       AuroraExtensions_SimpleReturns
+ * @copyright     Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
+ * @license       Aurora Extensions EULA
+ */
+namespace AuroraExtensions\SimpleReturns\Model;
+
+use AuroraExtensions\SimpleReturns\{
+    Api\Data\SimpleReturnInterface,
+    Shared\ModuleComponentInterface
+};
+use Magento\Framework\Model\AbstractModel;
+
+class SimpleReturn extends AbstractModel implements
+    SimpleReturnInterface,
+    ModuleComponentInterface
+{
+    /**
+     * @return void
+     */
+    public function _construct()
+    {
+        $this->_init(ResourceModel\SimpleReturn::class);
+    }
+}
