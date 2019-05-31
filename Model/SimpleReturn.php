75dabc353bf65763cd7db3fadbe7de65d19fece8
Nickolas Burr <nickolasburr@gmail.com>
2019-05-31 13:05:52 -0400

    Work on SimpleReturn data model

--- Model/SimpleReturn.php
+++ Model/SimpleReturn.php
@@ -16,13 +16,19 @@
  * @copyright     Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
  * @license       Aurora Extensions EULA
  */
+declare(strict_types=1);
+
 namespace AuroraExtensions\SimpleReturns\Model;
 
 use AuroraExtensions\SimpleReturns\{
     Api\Data\SimpleReturnInterface,
     Shared\ModuleComponentInterface
 };
-use Magento\Framework\Model\AbstractModel;
+
+use Magento\{
+    Framework\Model\AbstractModel,
+    Sales\Api\Data\OrderInterface
+};
 
 class SimpleReturn extends AbstractModel implements
     SimpleReturnInterface,
@@ -34,5 +40,45 @@ class SimpleReturn extends AbstractModel implements
     public function _construct()
     {
         $this->_init(ResourceModel\SimpleReturn::class);
+    }
+
+    /**
+     * @return OrderInterface
+     */
+    public function getOrder(): OrderInterface
+    {
+        return $this->getData('order');
+    }
+
+    /**
+     * @param OrderInterface $order
+     * @return $this
+     */
+    public function setOrder(OrderInterface $order): SimpleReturnInterface
+    {
+        $this->setData('order', $order);
+
+        return $this;
+    }
+
+    /**
+     * @return PackageInterface[]
+     */
+    public function getPackages(): array
+    {
+        return $this->getData('packages');
+    }
+
+    /**
+     * @param PackageInterface[] $packages
+     * @return $this
+     */
+    public function setPackages(
+        array $packages = []
+    ): SimpleReturnInterface
+    {
+        $this->setData('packages', $packages);
+
+        return $this;
     }
 }
