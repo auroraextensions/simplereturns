a734e6f66cb5d74b99f478d775330296a9c00a46
Nickolas Burr <nickolasburr@gmail.com>
2019-06-15 15:19:43 -0400

    Add returns/rma/create view model

---
+++ Model/ViewModel/Rma/CreateView.php
@@ -0,0 +1,62 @@
+<?php
+/** 
+ * CreateView.php
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Aurora Extensions EULA,
+ * which is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/simplereturns/LICENSE.txt
+ *
+ * @package       AuroraExtensions_SimpleReturns
+ * @copyright     Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
+ * @license       Aurora Extensions EULA
+ */ 
+declare(strict_types=1);
+
+namespace AuroraExtensions\SimpleReturns\Model\ViewModel\Rma;
+
+use AuroraExtensions\SimpleReturns\{
+    Exception\ExceptionFactory,
+    Helper\Action as ActionHelper,
+    Helper\Config as ConfigHelper,
+    Model\ViewModel\AbstractView,
+    Shared\ModuleComponentInterface
+};
+use Magento\Framework\{
+    App\RequestInterface,
+    UrlInterface,
+    View\Element\Block\ArgumentInterface
+};
+use Magento\Sales\Api\Data\OrderInterface;
+
+class CreateView extends AbstractView implements
+    ArgumentInterface,
+    ModuleComponentInterface
+{
+    /**
+     * @param ConfigHelper $configHelper
+     * @param ExceptionFactory $exceptionFactory
+     * @param RequestInterface $request
+     * @param UrlInterface $urlBuilder
+     * @param array $data
+     * @return void
+     */
+    public function __construct(
+        ConfigHelper $configHelper,
+        ExceptionFactory $exceptionFactory,
+        RequestInterface $request,
+        UrlInterface $urlBuilder,
+        array $data = []
+    ) {
+        parent::__construct(
+            $configHelper,
+            $exceptionFactory,
+            $request,
+            $urlBuilder,
+            $data
+        );
+    }
+}
