7881a887bac41ec5dc8f851ec2872ebe89d562ce
Nickolas Burr <nickolasburr@gmail.com>
2019-07-04 23:59:55 -0400

    Add InvalidCarrierException class

---
+++ Exception/InvalidCarrierException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * InvalidCarrierException.php
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Aurora Extensions EULA,
+ * which is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/simplereturns/LICENSE.txt
+ *
+ * @package       AuroraExtensions_SimpleReturns
+ * @copyright     Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
+ * @license       Aurora Extensions EULA
+ */
+namespace AuroraExtensions\SimpleReturns\Exception;
+
+class InvalidCarrierException extends \Exception
+{
+}
