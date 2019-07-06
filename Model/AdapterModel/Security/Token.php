4fe83bd79675245dcb2107f7662297bfb5a977f5
Nickolas Burr <nickolasburr@gmail.com>
2019-07-06 17:27:23 -0400

    Add Token adapter model

---
+++ Model/AdapterModel/Security/Token.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Token.php
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Aurora Extensions EULA,
+ * which is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/simplereturns/LICENSE.txt
+ *
+ * @package        AuroraExtensions_SimpleReturns
+ * @copyright      Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
+ * @license        Aurora Extensions EULA
+ */
+declare(strict_types=1);
+
+namespace AuroraExtensions\SimpleReturns\Model\AdapterModel\Security;
+
+use Magento\Framework\Math\Random as Generator;
+
+class Token
+{
+    /** @constant string HASH_ALGO */
+    const HASH_ALGO = 'sha512';
+
+    /** @property Generator $generator */
+    protected $generator;
+
+    /**
+     * @param Generator $generator
+     * @return void
+     */
+    public function __construct(Generator $generator)
+    {
+        $this->generator = $generator;
+    }
+
+    /**
+     * Create alphanumeric token of specified length.
+     *
+     * @param int $length
+     * @return string
+     */
+    public function createToken(int $length = 64): string
+    {
+        return $this->generator->getRandomString($length);
+    }
+
+    /**
+     * Get SHA-512 hash value for token.
+     *
+     * @param string $token
+     * @return string
+     */
+    public static function getHash(string $token): string
+    {
+        return hash(self::HASH_ALGO, $token);
+    }
+
+    /**
+     * Test computed hash equality.
+     *
+     * @param string $one
+     * @param string $two
+     * @return bool
+     */
+    public static function isEqual(string $one, string $two): bool
+    {
+        return hash_equals($one, $two);
+    }
+
+    /**
+     * Determine if token is alphanumeric.
+     *
+     * @param string $token
+     * @return bool
+     */
+    public static function isAlphanumeric(string $token): bool
+    {
+        return !preg_match('/[^A-Za-z0-9]/', $token);
+    }
+}
