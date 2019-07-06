fdada67fe0d5a5229fa88fc0f6fa7d1fb22cbd5e
Nickolas Burr <nickolasburr@gmail.com>
2019-07-06 00:23:52 -0400

    Work on simplereturns_rma_createPost controller

--- Controller/Rma/CreatePost.php
+++ Controller/Rma/CreatePost.php
@@ -19,20 +19,22 @@ declare(strict_types=1);
 namespace AuroraExtensions\SimpleReturns\Controller\Rma;
 
 use AuroraExtensions\SimpleReturns\{
+    Api\Data\SimpleReturnInterface,
+    Api\Data\SimpleReturnInterfaceFactory,
+    Api\SimpleReturnRepositoryInterface,
     Exception\ExceptionFactory,
     Model\AdapterModel\Sales\Order as OrderAdapter,
     Shared\Action\Redirector,
     Shared\ModuleComponentInterface
 };
-use Magento\Customer\Api\CustomerRepositoryInterface;
 use Magento\Framework\{
     App\Action\Action,
     App\Action\Context,
     App\Action\HttpPostActionInterface,
-    App\Request\DataPersistorInterface,
     Controller\Result\Redirect as ResultRedirect,
     Data\Form\FormKey\Validator as FormKeyValidator,
-    Exception\LocalizedException
+    Exception\LocalizedException,
+    Exception\NoSuchEntityException
 };
 
 class CreatePost extends Action implements
@@ -44,12 +46,6 @@ class CreatePost extends Action implements
         Redirector::__initialize as protected;
     }
 
-    /** @property CustomerRepositoryInterface $customerRepository */
-    protected $customerRepository;
-
-    /** @property DataPersistorInterface $dataPersistor */
-    protected $dataPersistor;
-
     /** @property ExceptionFactory $exceptionFactory */
     protected $exceptionFactory;
 
@@ -59,30 +55,36 @@ class CreatePost extends Action implements
     /** @property OrderAdapter $orderAdapter */
     protected $orderAdapter;
 
+    /** @property SimpleReturnInterfaceFactory $simpleReturnFactory */
+    protected $simpleReturnFactory;
+
+    /** @property SimpleReturnRepositoryInterface $simpleReturnRepository */
+    protected $simpleReturnRepository;
+
     /**
      * @param Context $context
-     * @param CustomerRepositoryInterface $customerRepository
-     * @param DataPersistorInterface $dataPersistor
      * @param ExceptionFactory $exceptionFactory
      * @param FormKeyValidator $formKeyValidator
      * @param OrderAdapter $orderAdapter
+     * @param SimpleReturnInterfaceFactory $simpleReturnFactory
+     * @param SimpleReturnRepositoryInterface $simpleReturnRepository
      * @return void
      */
     public function __construct(
         Context $context,
-        CustomerRepositoryInterface $customerRepository,
-        DataPersistorInterface $dataPersistor,
         ExceptionFactory $exceptionFactory,
         FormKeyValidator $formKeyValidator,
-        OrderAdapter $orderAdapter
+        OrderAdapter $orderAdapter,
+        SimpleReturnInterfaceFactory $simpleReturnFactory,
+        SimpleReturnRepositoryInterface $simpleReturnRepository
     ) {
         parent::__construct($context);
         $this->__initialize();
-        $this->customerRepository = $customerRepository;
-        $this->dataPersistor = $dataPersistor;
         $this->exceptionFactory = $exceptionFactory;
         $this->formKeyValidator = $formKeyValidator;
         $this->orderAdapter = $orderAdapter;
+        $this->simpleReturnFactory = $simpleReturnFactory;
+        $this->simpleReturnRepository = $simpleReturnRepository;
     }
 
     /**
@@ -106,6 +108,86 @@ class CreatePost extends Action implements
             /** @var int|string $orderId */
             $orderId = $params['order_id'] ?? null;
             $orderId = !empty($orderId) ? $orderId : null;
+
+            /** @var string $protectCode */
+            $protectCode = $params['protect_code'] ?? null;
+            $protectCode = !empty($protectCode) ? $protectCode : null;
+
+            /** @var string $reason */
+            $reason = $params['reason'] ?? null;
+            $reason = !empty($reason) ? $reason : null;
+
+            /** @var string $resolution */
+            $resolution = $params['resolution'] ?? null;
+            $resolution = !empty($resolution) ? $resolution : null;
+
+            /** @var string $comments */
+            $comments = $params['comments'] ?? null;
+            $comments = !empty($comments) ? $comments : null;
+
+            /** @var array $fields */
+            $fields = [
+                self::FIELD_INCREMENT_ID => $orderId,
+                self::FIELD_PROTECT_CODE => $protectCode,
+            ];
+
+            try {
+                /** @var OrderInterface[] $orders */
+                $orders = $this->orderAdapter->getOrdersByFields($fields);
+
+                if (!empty($orders)) {
+                    /** @var OrderInterface $order */
+                    $order = $orders[0];
+
+                    try {
+                        /** @var SimpleReturn $rma */
+                        $rma = $this->simpleReturnRepository->get($order);
+
+                        /** @note Consider possible redirect to RMA view page. */
+                        if ($rma->getId()) {
+                            /** @var LocalizedException $exception */
+                            $exception = $this->exceptionFactory->create(
+                                LocalizedException::class,
+                                __('An RMA request already exists for this order.')
+                            );
+
+                            throw $exception;
+                        }
+                    } catch (LocalizedException $e) {
+                        throw $e;
+                    } catch (NoSuchEntityException $e) {
+                        /* RMA doesn't exist, so continue processing. */
+                    }
+
+                    /** @var SimpleReturn $rma */
+                    $rma = $this->simpleReturnFactory->create();
+
+                    /** @var array $data */
+                    $data = [
+                        'order_id'   => $orderId,
+                        'reason'     => $reason,
+                        'resolution' => $resolution,
+                        'comments'   => $comments,
+                    ];
+
+                    $this->simpleReturnRepository->save(
+                        $rma->addData($data)
+                    );
+                }
+
+                /** @var LocalizedException $exception */
+                $exception = $this->exceptionFactory->create(
+                    LocalizedException::class,
+                    __('Unable to create RMA request.')
+                );
+
+                throw $exception;
+            } catch (LocalizedException $e) {
+                $this->messageManager->addError($e->getMessage());
+            } catch (NoSuchEntityException $e) {
+                $this->messageManager->addError($e->getMessage());
+            }
+
 
             /**
              * @todo: Check for existing RMA, redirect if exists.
