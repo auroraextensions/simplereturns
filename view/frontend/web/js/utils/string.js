810d4a6cb04512a3109a81e22dd5b844480a72eb
Nickolas Burr <nickolasburr@gmail.com>
2019-08-03 14:45:14 -0400

    Add utils/string.js

---
+++ view/frontend/web/js/utils/string.js
@@ -0,0 +1,39 @@
+/**
+ * string.js
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Aurora Extensions EULA,
+ * which is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/simplereturns/LICENSE.txt
+ *
+ * @package       AuroraExtensions_SimpleReturns
+ * @copyright     Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
+ * @license       Aurora Extensions EULA
+ */
+define([], function () {
+    'use strict';
+
+    return {
+        /**
+         * @param {String} value
+         * @param {String} delim
+         * @return {String}
+         */
+        trim: function (value, delim) {
+            delim = delim || '/';
+
+            while (value.charAt(0) === delim) {
+                value = value.slice(1);
+            }
+
+            while (value.charAt(value.length - 1) === delim) {
+                value = value.slice(0, value.length - 1);
+            }
+
+            return value;
+        }
+    };
+});
