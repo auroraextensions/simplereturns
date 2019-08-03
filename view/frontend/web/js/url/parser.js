6221541dce7a009f7b56a730d3dc03a20f9621a4
Nickolas Burr <nickolasburr@gmail.com>
2019-08-03 15:59:20 -0400

    Add url/parser.js

---
+++ view/frontend/web/js/url/parser.js
@@ -0,0 +1,64 @@
+/**
+ * parser.js
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Aurora Extensions EULA,
+ * which is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/simplereturns/LICENSE.txt
+ *
+ * @package       AuroraExtensions_SimpleReturns
+ * @copyright     Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
+ * @license       Aurora Extensions EULA
+ */
+define([
+    'jquery',
+    'AuroraExtensions_SimpleReturns/js/utils/string'
+], function ($, stringUtil) {
+    'use strict';
+
+    return {
+        /**
+         * @param {String} key
+         * @param {String} url
+         * @return {Boolean}
+         */
+        hasParam: function (key, url) {
+            url = url || document.location.pathname;
+
+            return !!(url.split(key).filter(Boolean).length - 1);
+        },
+        /**
+         * @param {String} key
+         * @param {String} url
+         * @return {String}
+         */
+        getParamValue: function (key, url) {
+            var index, parts, value;
+
+            url = url || document.location.pathname;
+            url = stringUtil.trim(url);
+
+            if (!this.hasParam(key, url)) {
+                return null;
+            }
+
+            /** @var {Array} parts */
+            parts = url.split('/').filter(Boolean);
+
+            /** @var {Number} index */
+            index = parts.indexOf(key);
+
+            if (index !== -1) {
+                /** @var {String} value */
+                value = parts.slice(index)[1];
+
+                return value ? value : null;
+            }
+
+            return null;
+        }
+    };
+});
