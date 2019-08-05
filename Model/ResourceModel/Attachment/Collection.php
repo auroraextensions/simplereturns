f9628e5ef26d88abd44ec4e5949a8740e7d5a858
Nickolas Burr <nickolasburr@gmail.com>
2019-08-04 21:54:37 -0400

    Add Attachment resource model, collection model

---
+++ Model/ResourceModel/Attachment/Collection.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Collection.php
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Aurora Extensions EULA,
+ * which is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/simplereturns/LICENSE.txt
+ *
+ * @package       AuroraExtensions_SimpleReturns
+ * @copyright     Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
+ * @license       Aurora Extensions EULA
+ */
+declare(strict_types=1);
+
+namespace AuroraExtensions\SimpleReturns\Model\ResourceModel\Attachment;
+
+use AuroraExtensions\SimpleReturns\{
+    Api\AbstractCollectionInterface,
+    Model\DataModel\Attachment,
+    Model\ResourceModel\Attachment as AttachmentResource
+};
+use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
+
+class Collection extends AbstractCollection implements AbstractCollectionInterface
+{
+    /**
+     * @return void
+     */
+    protected function _construct()
+    {
+        $this->_init(
+            Attachment::class,
+            AttachmentResource::class
+        );
+    }
+}
