19b75bbe857710ad3dccf2c238c37260ec315d56
Nickolas Burr <nickolasburr@gmail.com>
2019-08-12 01:26:38 -0400

    Add simplereturns_rma_list controller

---
+++ Controller/Adminhtml/Rma/List.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * List.php
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Aurora Extensions EULA,
+ * which is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/simplereturns/LICENSE.txt
+ *
+ * @package       AuroraExtensions_SimpleReturns
+ * @copyright     Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
+ * @license       Aurora Extensions EULA
+ */
+declare(strict_types=1);
+
+namespace AuroraExtensions\SimpleReturns\Controller\Adminhtml\Rma;
+
+use AuroraExtensions\SimpleReturns\Shared\ModuleComponentInterface;
+use Magento\Framework\{
+    App\Action\Action,
+    App\Action\Context,
+    App\Action\HttpGetActionInterface,
+    View\Result\PageFactory
+};
+
+class List extends Action implements
+    HttpGetActionInterface,
+    ModuleComponentInterface
+{
+    /** @property PageFactory $resultPageFactory */
+    protected $resultPageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     * @return void
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Execute simplereturns_rma_view action.
+     *
+     * @return Page
+     */
+    public function execute()
+    {
+        return $this->resultPageFactory->create();
+    }
+}
