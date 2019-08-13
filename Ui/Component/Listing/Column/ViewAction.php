7607f3a58502a17e803a67842f388f6d4f6e9635
Nickolas Burr <nickolasburr@gmail.com>
2019-08-13 17:24:47 -0400

    Add ViewAction UI component class

---
+++ Ui/Component/Listing/Column/ViewAction.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * ViewAction.php
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Aurora Extensions EULA,
+ * which is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/simplereturns/LICENSE.txt
+ *
+ * @package       AuroraExtensions_SimpleReturns
+ * @copyright     Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
+ * @license       Aurora Extensions EULA
+ */
+declare(strict_types=1);
+
+namespace AuroraExtensions\SimpleReturns\Ui\Component\Listing\Column;
+
+use Magento\Framework\{
+    UrlInterface,
+    View\Element\UiComponent\ContextInterface,
+    View\Element\UiComponentFactory
+};
+use Magento\Ui\Component\Listing\Columns\Column;
+
+class ViewAction extends Column
+{
+    /** @property UrlInterface $urlBuilder */
+    protected $urlBuilder;
+
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        UrlInterface $urlBuilder,
+        array $components = [],
+        array $data = []
+    ) {
+        parent::__construct(
+            $context,
+            $uiComponentFactory,
+            $urlBuilder,
+            $components,
+            $data
+        );
+    }
+
+    /**
+     * @param array $dataSource
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as &$item) {
+                if (isset($item['rma_id'])) {
+                    $viewUrlPath = $this->getData('config/viewUrlPath') ?: '#';
+                    $urlEntityParamName = $this->getData('config/urlEntityParamName') ?: 'rma_id';
+                    $item[$this->getData('name')] = [
+                        'view' => [
+                            'href' => $this->urlBuilder->getUrl(
+                                $viewUrlPath,
+                                [
+                                    $urlEntityParamName => $item['rma_id']
+                                ]
+                            ),
+                            'label' => __('View')
+                        ]
+                    ];
+                }
+            }
+        }
+
+        return $dataSource;
+    }
+}
