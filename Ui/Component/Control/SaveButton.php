790561d42fe0a205ad6279944b41aa834edbc6ca
Nickolas Burr <nickolasburr@gmail.com>
2019-08-24 13:58:16 -0400

    Add UI component SaveButton class

---
+++ Ui/Component/Control/SaveButton.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * SaveButton.php
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Aurora Extensions EULA,
+ * which is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/simplereturns/LICENSE.txt
+ *
+ * @package       AuroraExtensions_SimpleReturns
+ * @copyright     Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
+ * @license       Aurora Extensions EULA
+ */
+declare(strict_types=1);
+
+namespace AuroraExtensions\SimpleReturns\Ui\Component\Control;
+
+use Magento\Framework\{
+    App\RequestInterface,
+    Escaper,
+    UrlInterface,
+    View\Element\UiComponent\Control\ButtonProviderInterface
+};
+
+class SaveButton implements ButtonProviderInterface
+{
+    /** @property Escaper $escaper */
+    protected $escaper;
+
+    /** @property RequestInterface $request */
+    protected $request;
+
+    /** @property UrlInterface $urlBuilder */
+    protected $urlBuilder;
+
+    /**
+     * @param Escaper $escaper
+     * @param RequestInterface $request
+     * @param UrlInterface $urlBuilder
+     * @return void
+     */
+    public function __construct(
+        Escaper $escaper,
+        RequestInterface $request,
+        UrlInterface $urlBuilder
+    ) {
+        $this->escaper = $escaper;
+        $this->request = $request;
+        $this->urlBuilder = $urlBuilder;
+    }
+
+    /**
+     * @return array
+     */
+    public function getButtonData()
+    {
+        return [
+            'label' => __('Save'),
+            'class' => 'save primary',
+            'sort_order' => 10,
+        ];
+    }
+}
