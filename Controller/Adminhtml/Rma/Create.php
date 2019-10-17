d6f46eaf635da1a49b7154d034945b1f2df36b5f
Nickolas Burr <nickolasburr@gmail.com>
2019-10-16 23:23:59 -0400

    Add adminhtml simplereturns_rma_create controller

---
+++ Controller/Adminhtml/Rma/Create.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Create.php
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License, which
+ * is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/simplereturns/LICENSE.txt
+ *
+ * @package       AuroraExtensions_SimpleReturns
+ * @copyright     Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
+ * @license       MIT License
+ */
+declare(strict_types=1);
+
+namespace AuroraExtensions\SimpleReturns\Controller\Adminhtml\Rma;
+
+use AuroraExtensions\SimpleReturns\Shared\ModuleComponentInterface;
+use Magento\Backend\{
+    App\Action,
+    App\Action\Context
+};
+use Magento\Framework\{
+    App\Action\HttpGetActionInterface,
+    View\Result\PageFactory
+};
+
+class Create extends Action implements
+    HttpGetActionInterface,
+    ModuleComponentInterface
+{
+    /** @property PageFactory $resultPageFactory */
+    protected $resultPageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     * @return void
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * @return Page
+     */
+    public function execute()
+    {
+        /** @var Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->getConfig()->getTitle()->prepend(__('Create New RMA'));
+
+        return $resultPage;
+    }
+}
