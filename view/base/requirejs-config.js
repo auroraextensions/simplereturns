4ded6d25204f43b5168d6a3fd59f72591387e634
Nickolas Burr <nickolasburr@gmail.com>
2019-10-17 00:10:37 -0400

    Move dropzone.js into base area, add base area requirejs-config.js

---
+++ view/base/requirejs-config.js
@@ -0,0 +1,25 @@
+/**
+ * requirejs-config.js
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License, which
+ * is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/simplereturns/LICENSE.txt
+ *
+ * @package       AuroraExtensions_SimpleReturns
+ * @copyright     Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
+ * @license       MIT License
+ */
+var config = {
+    paths: {
+        'dropzone': 'AuroraExtensions_SimpleReturns/js/plugins/dropzone'
+    },
+    map: {
+        '*': {
+            simpleReturnsDragAndDrop: 'AuroraExtensions_SimpleReturns/js/dnd'
+        }
+    }
+};
