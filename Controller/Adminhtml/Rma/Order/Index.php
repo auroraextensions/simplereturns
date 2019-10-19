75527bf3c438d55f71afdb82d9320acdd1144a1e
Nickolas Burr <nickolasburr@gmail.com>
2019-10-19 15:33:22 -0400

    Add adminhtml simplereturns_rma_order_index controller

---
+++ Controller/Adminhtml/Rma/Order/Index.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Index.php
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License, which
+ * is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/simplereturns/LICENSE.txt
+ *
+ * @package       AuroraExtensions_SimpleReturns
+ * @copyright     Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
+ * @license       MIT License
+ */
+declare(strict_types=1);
+
+namespace AuroraExtensions\SimpleReturns\Controller\Adminhtml\Rma\Order;
+
+use AuroraExtensions\SimpleReturns\Shared\ModuleComponentInterface;
+use Magento\Backend\{
+    App\Action,
+    App\Action\Context
+};
+use Magento\Framework\{
+    App\Action\HttpGetActionInterface,
+    App\Action\HttpPostActionInterface,
+    View\Result\PageFactory
+};
+
+class Index extends Action implements
+    HttpGetActionInterface,
+    ModuleComponentInterface
+{
+    /** @constant string ADMIN_RESOURCE */
+    public const ADMIN_RESOURCE = 'AuroraExtensions_SimpleReturns::rma';
+
+    /** @property PageFactory $resultPageFactory */
+    protected $resultPageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     * @return void
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Execute simplereturns_rma_view action.
+     *
+     * @return Page
+     */
+    public function execute()
+    {
+        /** @var Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->getConfig()->getTitle()->prepend(__('Select Order for RMA'));
+
+        return $resultPage;
+    }
+}
