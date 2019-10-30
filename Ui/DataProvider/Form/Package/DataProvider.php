91bebb6054d05bf206b0f8770b574870af7af3cc
Nickolas Burr <nickolasburr@gmail.com>
2019-10-30 16:18:22 -0400

    Create Package UI component data provider class

---
+++ Ui/DataProvider/Form/Package/DataProvider.php
@@ -0,0 +1,199 @@
+<?php
+/**
+ * DataProvider.php
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License, which
+ * is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/simplereturns/LICENSE.txt
+ *
+ * @package       AuroraExtensions_SimpleReturns
+ * @copyright     Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
+ * @license       MIT License
+ */
+declare(strict_types=1);
+
+namespace AuroraExtensions\SimpleReturns\Ui\DataProvider\Form\Package;
+
+use Countable;
+use AuroraExtensions\SimpleReturns\{
+    Model\ResourceModel\Package as PackageResource,
+    Model\ResourceModel\Package\Collection,
+    Model\ResourceModel\Package\CollectionFactory,
+    Shared\ModuleComponentInterface
+};
+use Magento\Framework\{
+    Api\FilterBuilder,
+    Api\Search\SearchCriteria,
+    Api\Search\SearchCriteriaBuilder,
+    Api\Search\SearchResultInterface,
+    App\RequestInterface,
+    View\Element\UiComponent\DataProvider\DataProviderInterface
+};
+use Magento\Ui\DataProvider\AbstractDataProvider;
+
+class DataProvider extends AbstractDataProvider implements
+    Countable,
+    DataProviderInterface,
+    ModuleComponentInterface
+{
+    /** @constant string WILDCARD */
+    public const WILDCARD = '*';
+
+    /** @property FilterBuilder $filterBuilder */
+    protected $filterBuilder;
+
+    /** @property array $labels */
+    protected $labels;
+
+    /** @property array $loadedData */
+    protected $loadedData = [];
+
+    /** @property RequestInterface $request */
+    protected $request;
+
+    /** @property SearchCriteriaBuilder $searchCriteriaBuilder */
+    protected $searchCriteriaBuilder;
+
+    /**
+     * @param string $name
+     * @param string $primaryFieldName
+     * @param string $requestFieldName
+     * @param array $meta
+     * @param array $data
+     * @param CollectionFactory $collectionFactory
+     * @param FilterBuilder $filterBuilder
+     * @param array $labels
+     * @param RequestInterface $request
+     * @param SearchCriteriaBuilder $searchCriteriaBuilder
+     * @return void
+     */
+    public function __construct(
+        $name,
+        $primaryFieldName,
+        $requestFieldName,
+        array $meta = [],
+        array $data = [],
+        CollectionFactory $collectionFactory,
+        FilterBuilder $filterBuilder,
+        array $labels = [],
+        RequestInterface $request,
+        SearchCriteriaBuilder $searchCriteriaBuilder
+    ) {
+        parent::__construct(
+            $name,
+            $primaryFieldName,
+            $requestFieldName,
+            $meta,
+            $data
+        );
+        $this->collection = $collectionFactory->create();
+        $this->filterBuilder = $filterBuilder;
+        $this->labels = $labels;
+        $this->request = $request;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+        $this->prepareSubmitUrl();
+    }
+
+    /**
+     * @return void
+     */
+    protected function prepareSubmitUrl(): void
+    {
+        if (isset($this->data['config']['submit_url'])) {
+            $this->parseSubmitUrl();
+        }
+
+        if (isset($this->data['config']['filter_url_params'])) {
+            /** @var string $paramName */
+            /** @var mixed $paramValue */
+            foreach ($this->data['config']['filter_url_params'] as $paramName => $paramValue) {
+                $paramValue = $paramValue !== static::WILDCARD
+                    ? $paramValue
+                    : $this->request->getParam($paramName);
+
+                if ($paramValue) {
+                    $this->data['config']['submit_url'] = sprintf(
+                        '%s%s/%s/',
+                        $this->data['config']['submit_url'],
+                        $paramName,
+                        $paramValue
+                    );
+
+                    /** @var Filter $filter */
+                    $filter = $this->filterBuilder
+                        ->setField($paramName)
+                        ->setValue($paramValue)
+                        ->setConditionType('eq')
+                        ->create();
+
+                    $this->searchCriteriaBuilder->addFilter($filter);
+                }
+            }
+        }
+    }
+
+    /**
+     * @return void
+     */
+    protected function parseSubmitUrl(): void
+    {
+        /** @var string $actionName */
+        $actionName = strtolower($this->request->getActionName()) . 'Post';
+
+        /** @var string $submitUrl */
+        $submitUrl = $this->data['config']['submit_url'];
+
+        $this->data['config']['submit_url'] = str_replace(
+            ':action',
+            $actionName,
+            $submitUrl
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function getLabelKeys(): array
+    {
+        /** @var array $labels */
+        $labels = $this->labels ?? [];
+
+        return array_keys($labels);
+    }
+
+    /**
+     * @param bool $preserveKeys
+     * @return array
+     */
+    public function getLabels(bool $preserveKeys = true): array
+    {
+        /** @var array $labels */
+        $labels = $this->labels ?? [];
+
+        return $preserveKeys ? $labels : array_values($labels);
+    }
+
+    /**
+     * @return array
+     */
+    public function getData(): array
+    {
+        if (!empty($this->loadedData)) {
+            return $this->loadedData;
+        }
+
+        /** @var SimpleReturnInterface[] $items */
+        $items = $this->getCollection()->getItems();
+
+        /** @var PackageInterface $rma */
+        foreach ($items as $pkg) {
+            $this->loadedData[$pkg->getId()] = $pkg->getData();
+        }
+
+        return $this->loadedData;
+    }
+}
