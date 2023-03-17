462e7c1dd1964b689c4c753f1ef6ca5cd15131b8
Nickolas Burr <nickolasburr@gmail.com>
2023-03-16 23:06:34 -0400

    Extend MC AbstractRepositoryInterface in entity interfaces

--- Api/AttachmentRepositoryInterface.php
+++ Api/AttachmentRepositoryInterface.php
@@ -4,19 +4,21 @@
  *
  * NOTICE OF LICENSE
  *
- * This source file is subject to the MIT License, which
+ * This source file is subject to the MIT license, which
  * is bundled with this package in the file LICENSE.txt.
  *
  * It is also available on the Internet at the following URL:
  * https://docs.auroraextensions.com/magento/extensions/2.x/simplereturns/LICENSE.txt
  *
- * @package        AuroraExtensions_SimpleReturns
- * @copyright      Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
- * @license        MIT License
+ * @package      AuroraExtensions\SimpleReturns\Api
+ * @copyright    Copyright (C) 2023 Aurora Extensions <support@auroraextensions.com>
+ * @license      MIT
  */
 declare(strict_types=1);
 
 namespace AuroraExtensions\SimpleReturns\Api;
+
+use AuroraExtensions\ModuleComponents\Api\AbstractRepositoryInterface;
 
 interface AttachmentRepositoryInterface extends AbstractRepositoryInterface
 {
