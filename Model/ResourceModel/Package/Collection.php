11382917c0e92a92c7d4bd52ccfd2289e5359c57
Nickolas Burr <nickolasburr@gmail.com>
2023-03-16 23:08:53 -0400

    Utilize MC AbstractCollectionInterface in collection models

--- Model/ResourceModel/Package/Collection.php
+++ Model/ResourceModel/Package/Collection.php
@@ -4,25 +4,23 @@
  *
  * NOTICE OF LICENSE
  *
- * This source file is subject to the MIT License, which
+ * This source file is subject to the MIT license, which
  * is bundled with this package in the file LICENSE.txt.
  *
  * It is also available on the Internet at the following URL:
  * https://docs.auroraextensions.com/magento/extensions/2.x/simplereturns/LICENSE.txt
  *
- * @package       AuroraExtensions_SimpleReturns
- * @copyright     Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
- * @license       MIT License
+ * @package     AuroraExtensions\SimpleReturns\Model\ResourceModel\Package
+ * @copyright   Copyright (C) 2023 Aurora Extensions <support@auroraextensions.com>
+ * @license     MIT
  */
 declare(strict_types=1);
 
 namespace AuroraExtensions\SimpleReturns\Model\ResourceModel\Package;
 
-use AuroraExtensions\SimpleReturns\{
-    Api\AbstractCollectionInterface,
-    Model\DataModel\Package as PackageDataModel,
-    Model\ResourceModel\Package as PackageResourceModel
-};
+use AuroraExtensions\ModuleComponents\Api\AbstractCollectionInterface;
+use AuroraExtensions\SimpleReturns\Model\DataModel\Package;
+use AuroraExtensions\SimpleReturns\Model\ResourceModel\Package as PackageResource;
 use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
 
 class Collection extends AbstractCollection implements AbstractCollectionInterface
@@ -33,8 +31,8 @@ class Collection extends AbstractCollection implements AbstractCollectionInterfa
     protected function _construct()
     {
         $this->_init(
-            PackageDataModel::class,
-            PackageResourceModel::class
+            Package::class,
+            PackageResource::class
         );
     }
 }
--- Model/ResourceModel/SimpleReturn/Collection.php
+++ Model/ResourceModel/SimpleReturn/Collection.php
@@ -4,25 +4,23 @@
  *
  * NOTICE OF LICENSE
  *
- * This source file is subject to the MIT License, which
+ * This source file is subject to the MIT license, which
  * is bundled with this package in the file LICENSE.txt.
  *
  * It is also available on the Internet at the following URL:
  * https://docs.auroraextensions.com/magento/extensions/2.x/simplereturns/LICENSE.txt
  *
- * @package       AuroraExtensions_SimpleReturns
- * @copyright     Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
- * @license       MIT License
+ * @package     AuroraExtensions\SimpleReturns\Model\ResourceModel\SimpleReturn
+ * @copyright   Copyright (C) 2023 Aurora Extensions <support@auroraextensions.com>
+ * @license     MIT
  */
 declare(strict_types=1);
 
 namespace AuroraExtensions\SimpleReturns\Model\ResourceModel\SimpleReturn;
 
-use AuroraExtensions\SimpleReturns\{
-    Api\AbstractCollectionInterface,
-    Model\DataModel\SimpleReturn,
-    Model\ResourceModel\SimpleReturn as SimpleReturnResource
-};
+use AuroraExtensions\ModuleComponents\Api\AbstractCollectionInterface;
+use AuroraExtensions\SimpleReturns\Model\DataModel\SimpleReturn;
+use AuroraExtensions\SimpleReturns\Model\ResourceModel\SimpleReturn as SimpleReturnResource;
 use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
 
 class Collection extends AbstractCollection implements AbstractCollectionInterface
