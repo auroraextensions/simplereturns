88ebfcd7540f1e8b6f890107ed7fc0adbd698e96
Nickolas Burr <nickolasburr@gmail.com>
2023-03-25 23:31:12 -0400

    Refactor adminhtml Package Create controller

--- Controller/Adminhtml/Package/Create.php
+++ Controller/Adminhtml/Package/Create.php
@@ -4,36 +4,32 @@
  *
  * NOTICE OF LICENSE
  *
- * This source file is subject to the MIT License, which
+ * This source file is subject to the MIT license, which
  * is bundled with this package in the file LICENSE.txt.
  *
  * It is also available on the Internet at the following URL:
  * https://docs.auroraextensions.com/magento/extensions/2.x/simplereturns/LICENSE.txt
  *
- * @package       AuroraExtensions_SimpleReturns
- * @copyright     Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
- * @license       MIT License
+ * @package     AuroraExtensions\SimpleReturns\Controller\Adminhtml\Package
+ * @copyright   Copyright (C) 2023 Aurora Extensions <support@auroraextensions.com>
+ * @license     MIT
  */
 declare(strict_types=1);
 
 namespace AuroraExtensions\SimpleReturns\Controller\Adminhtml\Package;
 
-use AuroraExtensions\SimpleReturns\Shared\ModuleComponentInterface;
-use Magento\Backend\{
-    App\Action,
-    App\Action\Context
-};
-use Magento\Framework\{
-    App\Action\HttpGetActionInterface,
-    View\Result\PageFactory
-};
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
 
-class Create extends Action implements
-    HttpGetActionInterface,
-    ModuleComponentInterface
+use function __;
+
+class Create extends Action implements HttpGetActionInterface
 {
-    /** @property PageFactory $resultPageFactory */
-    protected $resultPageFactory;
+    /** @var PageFactory $resultPageFactory */
+    private $resultPageFactory;
 
     /**
      * @param Context $context
@@ -55,7 +51,9 @@ class Create extends Action implements
     {
         /** @var Page $resultPage */
         $resultPage = $this->resultPageFactory->create();
-        $resultPage->getConfig()->getTitle()->prepend(__('Create Package'));
+        $resultPage->getConfig()
+            ->getTitle()
+            ->prepend(__('Create Package'));
 
         return $resultPage;
     }
